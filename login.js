5081cade2b2a320dd527986c4ee724929451e6d7
maaritklee-blip <maaritklee@gmail.com>
2026-06-29 15:15:17 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,34 @@
-console.log("Login geladen");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuXjoePXrmbBWtJJkVl8T2UVshli1O3MM",
+  authDomain: "mamataxi-da5aa.firebaseapp.com",
+  projectId: "mamataxi-da5aa",
+  storageBucket: "mamataxi-da5aa.firebasestorage.app",
+  messagingSenderId: "33580844045",
+  appId: "1:33580844045:web:e5baffa8c76073a7f54144"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginButton = document.getElementById("loginButton");
+const status = document.getElementById("status");
+
+loginButton.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    status.textContent = "✅ Erfolgreich angemeldet!";
+    window.location.href = "index.html";
+  } catch (error) {
+    status.textContent = "❌ Anmeldung fehlgeschlagen.";
+    console.error(error);
+  }
+});
